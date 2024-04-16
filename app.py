199dd55dfec80bc1d6caf2b0f793ad63ce5b3670
扈海波 <huhaibo@huhaibodeMacBook-Air.local>
2024-04-16 14:49:30 +0800

    update

--- app.py
+++ app.py
@@ -129,15 +129,3 @@ elif choose == '图片处理':
                 st.header("处理后的图片")
                 if result_img_dir is not None:
                     st.image(result_img_dir)    
-
-# js_code = '''
-# $(document).ready(function(){
-#     $("button[kind=icon]", window.parent.document).remove()
-# });
-# '''
-# # 因为JS不需要展示，所以html宽高均设为0，避免占用空间，且放置在所有组件最后
-# # 引用了JQuery v2.2.4
-# html(f'''<script src="https://cdn.bootcdn.net/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-#     <script>{js_code}</script>''',
-#      width=0,
-#      height=0)
